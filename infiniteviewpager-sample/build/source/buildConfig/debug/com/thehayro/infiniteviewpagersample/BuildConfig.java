4bf9d557ea8747d5130cfc60ab6b4f2e0e0fdf05
Gottox <g@s01.de>
2013-11-25 11:05:49 +0100

    delete build

--- infiniteviewpager-sample/build/source/buildConfig/debug/com/thehayro/infiniteviewpagersample/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.thehayro.infiniteviewpagersample;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
-}
