9fec3d425e0690b841b70be788ca20943fd15913
Gottox <g@s01.de>
2013-11-25 10:07:57 +0100

    move to gradle

---
+++ infiniteviewpager-sample/build/source/buildConfig/release/com/thehayro/infiniteviewpagersample/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.thehayro.infiniteviewpagersample;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = false;
+
+}
